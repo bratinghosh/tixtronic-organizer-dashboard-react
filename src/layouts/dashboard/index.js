caccfdb99b9cf6c02e9ab4d0176bc01949a271f9
bratinghosh <ghosh.bratin13@gmail.com>
2023-07-13 20:55:56 +0800

    update dashboard, add new elements

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -15,54 +15,56 @@ Coded by www.creative-tim.com
 
 /* eslint-disable */
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import axios from "axios";
 
 // @mui material components
 import Grid from "@mui/material/Grid";
+import Icon from "@mui/material/Icon";
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
 
 // Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
-import DashboardNavbar from "examples/Navbars/DashboardNavbar";
-import Footer from "examples/Footer";
+// import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+// import Footer from "examples/Footer";
 import ReportsBarChart from "examples/Charts/BarCharts/ReportsBarChart";
 import ReportsLineChart from "examples/Charts/LineCharts/ReportsLineChart";
+import PieChart from "examples/Charts/PieChart";
+
 import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatisticsCard";
 
-// Data
-import reportsBarChartData from "layouts/dashboard/data/reportsBarChartData";
-import reportsLineChartData from "layouts/dashboard/data/reportsLineChartData";
 
-
-// Dashboard components
-import Projects from "layouts/dashboard/components/Projects";
-import OrdersOverview from "layouts/dashboard/components/OrdersOverview";
 
 function Dashboard() {
-  const { sales, tasks } = reportsLineChartData;
-
   const [loading, setLoading] = useState(true);
-  const [totalMintsData, setTotalMintsData] = useState();
+
+  const [analytics, setAnalytics] = useState(undefined);
 
   useEffect(() => {
-    const baseUrl = "http://localhost:3001/analytics";
-    fetchDataHelper(baseUrl + "/mint/totalticketsminted", setTotalMintsData);
+    fetchDataHelper("http://localhost:3001/analytics", setAnalytics);
   }, []);
 
   const fetchDataHelper = async (url, setDataFunction) => {
     axios
       .get(url)
-      .then((response) => {
-        setLoading(true);
-        setDataFunction(response.data.data);
+      .then(async (response) => {
+        const json = response.data.data;
+        console.log(json);
+        setDataFunction({...json, "timestamp": ""});
         setLoading(false);
       })
       .catch((error) => {
         console.error(error);
       });
+  };
+
+  const chartHelper = (title, labels, data) => {
+    return {
+      labels: labels,
+      datasets: { label: title, data: data },
+    }
   };
 
   return (
@@ -71,36 +73,51 @@ function Dashboard() {
       <DashboardLayout>
       <MDBox py={3}>
         <Grid container spacing={3}>
-          <Grid item xs={12} md={6} lg={3}>
+          <Grid item xs={12} md={6} lg={4}>
             <MDBox mb={1.5}>
               <ComplexStatisticsCard
                 color="info"
                 icon="confirmation_number_icon"
-                title="Total Mints"
-                count={totalMintsData.totalticketsminted}
+                title="Total Tickets Minted"
+                count={analytics.totalticketsminted.data.totalticketsminted}
                 percentage={{
                   color: "success",
-                  amount: "+55%",
-                  label: "than lask week",
+                  amount: "",
+                  label: "Primary Market",
                 }}
               />
             </MDBox>
           </Grid>
-          <Grid item xs={12} md={6} lg={3}>
+          <Grid item xs={12} md={6} lg={4}>
             <MDBox mb={1.5}>
               <ComplexStatisticsCard
-                icon="leaderboard"
-                title="Today's Users"
-                count="2,300"
+                color="success"
+                icon="today"
+                title={"Tickets Minted Today (" + analytics.totalticketsmintedtoday.data.weekday + ")"}
+                count={analytics.totalticketsmintedtoday.data.totalticketsmintedtoday}
                 percentage={{
                   color: "success",
-                  amount: "+3%",
-                  label: "than last month",
+                  amount: "",
+                  label: "Primary Market",
                 }}
               />
             </MDBox>
           </Grid>
-          <Grid item xs={12} md={6} lg={3}>
+          <Grid item xs={12} md={6} lg={4}>
+            <MDBox mb={1.5}>
+              <PieChart
+                color="secondary"
+                title="Tickets On-Sale"
+                description="Secondary Market"
+                chart={
+                  chartHelper("Tickets On-Sale", 
+                              analytics.ticketsonsale.data.labels,
+                              analytics.ticketsonsale.data.ticketsonsale)
+                }
+              />
+            </MDBox>
+          </Grid>
+          {/* <Grid item xs={12} md={6} lg={4}>
             <MDBox mb={1.5}>
               <ComplexStatisticsCard
                 color="success"
@@ -114,71 +131,84 @@ function Dashboard() {
                 }}
               />
             </MDBox>
-          </Grid>
-          <Grid item xs={12} md={6} lg={3}>
+          </Grid> */}
+          {/* <Grid item xs={12} md={6} lg={4}>
             <MDBox mb={1.5}>
               <ComplexStatisticsCard
                 color="primary"
-                icon="person_add"
-                title="Followers"
-                count="+91"
+                icon="storefront"
+                title="Tickets On-Sale"
+                count={analytics.ticketsonsale.data.ticketsonsale}
                 percentage={{
                   color: "success",
                   amount: "",
-                  label: "Just updated",
+                  label: "Secondary Market",
                 }}
               />
             </MDBox>
-          </Grid>
+          </Grid> */}
         </Grid>
         <MDBox mt={4.5}>
           <Grid container spacing={3}>
-            <Grid item xs={12} md={6} lg={4}>
+            <Grid item xs={12} md={6} lg={6}>
               <MDBox mb={3}>
                 <ReportsBarChart
-                  color="info"
-                  title="website views"
-                  description="Last Campaign Performance"
-                  date="campaign sent 2 days ago"
-                  chart={reportsBarChartData}
+                  color="dark"
+                  title="Tickets Minted"
+                  description="by weekday"
+                  date="Primary Market"
+                  chart={
+                    chartHelper("Tickets Minted", 
+                                analytics.totalticketsmintedbyweekdays.data.weekdays,
+                                analytics.totalticketsmintedbyweekdays.data.totalticketsmintedbyweekdays)
+                  }
                 />
               </MDBox>
             </Grid>
-            <Grid item xs={12} md={6} lg={4}>
+            <Grid item xs={12} md={6} lg={6}>
+              <MDBox mb={3}>
+                <ReportsLineChart
+                  color="info"
+                  title="Tickets Minted"
+                  description="by day"
+                  date="Primary Market"
+                  chart={
+                    chartHelper("Tickets Minted", 
+                                analytics.totalticketsmintedbyday.data.days,
+                                analytics.totalticketsmintedbyday.data.totalticketsmintedbyday)
+                  }
+                />
+              </MDBox>
+            </Grid>
+            <Grid item xs={12} md={6} lg={6}>
               <MDBox mb={3}>
                 <ReportsLineChart
                   color="success"
-                  title="daily sales"
-                  description={
-                    <>
-                      (<strong>+15%</strong>) increase in today sales.
-                    </>
+                  title="Cumulative Tickets Minted"
+                  description="by day"
+                  date="Primary Market"
+                  chart={
+                    chartHelper("Tickets Minted", 
+                                analytics.cumulativeticketsmintedbyday.data.days,
+                                analytics.cumulativeticketsmintedbyday.data.cumulativeticketsmintedbyday)
                   }
-                  date="updated 4 min ago"
-                  chart={sales}
                 />
               </MDBox>
             </Grid>
-            <Grid item xs={12} md={6} lg={4}>
+            <Grid item xs={12} md={6} lg={6}>
               <MDBox mb={3}>
-                <ReportsLineChart
-                  color="dark"
-                  title="completed tasks"
-                  description="Last Campaign Performance"
-                  date="just updated"
-                  chart={tasks}
+                <ReportsBarChart
+                  color="primary"
+                  title="Tickets Owned"
+                  description="per user"
+                  date="General"
+                  chart={
+                    chartHelper("Tickets Minted", 
+                                analytics.ticketsperuserfrequency.data.ticketscount,
+                                analytics.ticketsperuserfrequency.data.frequency)
+                  }
                 />
               </MDBox>
-            </Grid>
-          </Grid>
-        </MDBox>
-        <MDBox>
-          <Grid container spacing={3}>
-            <Grid item xs={12} md={6} lg={8}>
-              <Projects />
-            </Grid>
-            <Grid item xs={12} md={6} lg={4}>
-              <OrdersOverview />
             </Grid>
           </Grid>
         </MDBox>
